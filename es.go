568be4f55337f84e7623b78d906084cc6cb86c71
17717353366 <songfeigang@dianbaobao.com>
2017-11-20 12:05:00 +0800

    处理规格化路由

--- es.go
+++ es.go
@@ -2,6 +2,7 @@ package ges
 
 import (
 	"net/http"
+	"strings"
 	"reflect"
 )
 
@@ -10,11 +11,26 @@ type Application struct {
 	Helper Helper
 }
 
+type ControllerInterface interface{
+	Begin()
+	After()
+	Display()
+}
+
 var App *Application
+var RouterMaps  map[string] *ControllerInterface
 
 func autoRoute(w http.ResponseWriter, r *http.Request)  {
 	requestPath := r.URL.Path
-	w.Write([]byte(requestPath))
+	paths := strings.Split(requestPath, "/");
+	rootPath := paths[0]+"/"+paths[1];
+	controller :=RouterMaps[rootPath]
+	if(controller != nil){
+		//reflect.TypeOf();
+		reflect.ValueOf(controller).MethodByName(paths[2]).Call([]reflect.Value{})
+	}else{
+		http.NotFoundHandler()
+	}
 }
 
 func static(w http.ResponseWriter, r *http.Request) {
@@ -22,11 +38,16 @@ func static(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, file)
 }
 
+func Router(rootpath string, c *ControllerInterface){
+	RouterMaps = make(map[string] *ControllerInterface)
+	RouterMaps[rootpath] = c
+}
+
 func  Run()  {
 	App := &Application{}
 	App.Helper = Helper{}
 	App.Static = App.Helper.GetConfig("server","static")
-	http.HandleFunc("/static/", static)
+	http.HandleFunc("/"+App.Static+"/", static)
 	http.HandleFunc("/" , autoRoute)
 	http.ListenAndServe(App.Helper.GetConfig("server","address"),nil)
 }
